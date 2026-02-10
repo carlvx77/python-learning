b26e21f472ba8795ef0118e9df058e1cb2cfb27c
carlvx77 <ghulkuh869@gmail.com>
2026-02-10 22:56:06 +0300

    first python backend commmit

---
+++ 3 deneme haklı giriş sistemi.py
@@ -0,0 +1,24 @@
+users = {
+    "carl": "1515",
+    "ege": "2525"
+}
+
+def login(u, p):
+    if u in users and users[u] == p:
+        return True, "giriş başarılı"
+    else:
+        return False, "hatalı bilgiler"
+
+hak = 3
+while hak > 0:
+    u = input("kullanıcı adınızı giriniz: ")
+    p = input("şifrenizi giriniz: ")
+    sonuc, mesaj = login(u, p)
+    if sonuc:
+        print("HOŞGELDİN REİS")
+        break
+    else:
+        hak -= 1
+        print(mesaj, "kalan deneme hakkı", hak)
+    if hak == 0:
+        print("hesap kilitlenmiştir")
